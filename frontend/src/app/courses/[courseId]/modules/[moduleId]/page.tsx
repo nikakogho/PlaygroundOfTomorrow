c698647a91aef28d5dd237cdbe25da8a6f4d8adb
nikakogho <nikakoghuashvili@gmail.com>
2025-06-12 18:12:17 +0400

    Removed unused page

--- frontend/src/app/courses/[courseId]/modules/[moduleId]/page.tsx
+++
@@ -1,6 +0,0 @@
-import ModulePage from '../../../ModulePage';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function Page({ params }: { params: any }) {
-  return <ModulePage params={params} />;
-}
